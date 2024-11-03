192412c401c0a1b2c369ae4331a018a647f17968
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-11-03 17:43:07 -0500

    Implement async/await to fetch ticket data and handle errors. TASK TWO

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 //java comment
 
+async function fetchTickets() {
+    const errorMessageDiv = document.getElementById('error-message');
+    const ticketContainer = document.getElementById('ticket-container');
+    
+    try {
+        ticketContainer.innerHTML = 'Awaiting response';
+
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        
+        
+        if (!response.ok) {
+            throw new Error('Response was not ok'); //checks if response is ok
+        }
+
+        const tickets = await response.json();
+
+        if (tickets.length === 0) { //checks if tickets are found if not throws a custom message
+            throw new Error('No tickets found');
+        }
+
+     catch (error) {
+        errorMessageDiv.textContent = `Error: ${error.message}`;
+        errorMessageDiv.style.display = 'block';
+    }
+}
